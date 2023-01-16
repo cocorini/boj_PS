f8274963a1458de29b0e2bb69bee8ae95223819d
공부해야하는 “꼴찌” 입니다 <108528528+cocorini@users.noreply.github.com>
2023-01-16 17:10:27 +0900

    boj_2042_구간 합 구하기
    #세그먼트 트리
    
    이전에 알고리즘 스터디를 할 때, 기본적인 개념은 설명을 들어서 알고 있는 개념이긴 했지만, 구현을 해본 것은 이번이 처음이라 상당히 어려움을 느꼈다.
    트리 자료 구조 임으로, 함수를 구현할 때 담아야 하는 인자로, 현재의 노드(tree 상에서), 구간의 시작, 끝은 반드시 포함되어 있어야 한다.
    세그먼트 트리를 이용해서 문제를 푸는 과정이 크게 3단계로 나눠져 있었다.
    
    1) 세그먼트 트리 초기화 (초기 구간합을 세그먼트 트리를 이용하여 구현)
    -세그먼트 트리의 노드 개수를 미리 할당해 놓자 -> 보통 간편하게, N*4로 트리의 노드 개수를 정해주지만,
    이번 문제는 처음 세그먼트 트리를 구현하기도 했고, 좀 더 정확하게 구현하기 위해 트리의 높이를 log(N/2)를 이용해서 먼저 구하고 올림 한 후에, 2^(h+1) 한 값을 사용했다.
    -mkST 함수를 이용한다(다른 예제의 init함수와 동일)
    재귀적으로 구현해야 하는 트리 자료구조임으로, 당연히 종료 조건이 존재함
    
    2) 구간의 값 변경
    - 종료 조건: 내가 변경할 값의 인덱스는 반드시 tree배열의 구간 시작과 끝 사이에 있어야 함으로, 그렇지 않은 경우는 return함.
    - 내가 변경한 값을 포함하는 구간은 모두 diff값 만큼 더해져야 함.
    - v배열의 해당 인덱스 값의 변경 또한 필요하다. (이것을 처음에 놓쳤는데, diff값을 구하기 위해서는 v배열도 갱신 되어야 함을 잊지 말자!!)
    3) 구간 합 구하기
    - 이 과정이 좀 헷갈렸는데, 의외로 간단했다.
    - 세그먼트 트리에서 들어간 구간과 합을 구해야 하는 구간의 관계성이 매우 중요했다.
    1. 세그먼트 트리에서의 해당 노드의 구간과 내가 구하려는 구간의 겹치는 부분이 전혀 없는 경우 -> return 0;
    2. 내가 구하려는 구간이 세그먼트 트리의 해당 구간에 완전히 포함되는 경우 -> return tree[node];
    3. else -> 다시 자식 노드들의 합을 return;

---
+++ boj_2042.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#define ll long long
+#define MAX 1000001
+using namespace std;
+void input();
+vector<ll> v(MAX, 0), tree;
+void solve(int a, int b, ll c);
+void insert(int node, int start, int end, int idx, ll diff);
+ll summation(int node, int start, int end, int section_start, int section_end);
+ll mkST(int node, int start, int end);
+int N, M, K, sizze;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	input();
+}
+void input() {
+	cin >> N >> M >> K;
+	int h = (int)ceil(log(N) / log(2));
+	sizze = (int)pow(2, h + 1);
+	tree.assign(sizze, 0);
+	for (int i = 1; i <= N; i++) cin >> v[i];
+	mkST(1, 1, N);
+	for (int j = 0; j < M + K; j++) {
+		int a, b;
+		ll c;
+		cin >> a >> b >> c;
+		solve(a, b, c);
+	}
+}
+void solve(int a, int b, ll c) {
+	//업데이트
+	if (a == 1) {
+		//cout << "v[b]는 " << v[b] << '\n';
+    //여기서 v[b]를 빼지않고, b를 빼서 계속해서 틀림...
+		insert(1, 1, N, b, c - v[b]);
+    //처음에는 v 배열 갱신도 안해줌..
+		v[b] = c;
+		//cout << "v[b]는 " << v[b] << '\n';
+		//for (int i = 1; i < sizze; i++) cout << tree[i] << ' ';
+		//cout << '\n';
+	}
+	//구간 합 출력하기
+	else if (a == 2) cout << summation(1, 1, N, b, c) << '\n';
+}
+ll mkST(int node, int start, int end) {
+	if (start == end) return tree[node] = v[start];
+	int mid = (start + end) / 2;
+	return tree[node] = mkST(node * 2, start, mid) + mkST(node * 2 + 1, mid + 1, end);
+}
+void insert(int node, int start, int end, int idx, ll diff) {
+	//leaf node라는 것을 이용해서 idx를 찾으려고 했음->불가능함..
+	//start와 end값은 v의 인덱스의 범위를 나타내는 것이므로, idx가 start와 end사이의 값이 아니라면 반환
+	if (!(start <= idx && idx <= end)) return;
+	tree[node] += diff;
+	if (start != end) {
+		int mid = (start + end) / 2;
+		insert(node * 2, start, mid, idx, diff);
+		insert(node * 2 + 1, mid + 1, end, idx, diff);
+	}
+}
+ll summation(int node, int start, int end, int section_start, int section_end) {
+	if (end < section_start || start > section_end) return 0;
+	else if (section_start <= start && end <= section_end) return tree[node];
+	int mid = (start + end) / 2;
+	return summation(node * 2, start, mid, section_start, section_end) + summation(node * 2 + 1, mid + 1, end, section_start, section_end);
+}
